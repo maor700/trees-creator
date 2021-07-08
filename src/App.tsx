43e8e4cea3ec6e208efaa61e03f0eb16eb5121e7
Maor Elimelech <maor700@gmail.com>
2021-07-08 16:14:03 +0300

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { UserLogin } from 'dexie-cloud-addon/dist/types/db/entities/UserLogin';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { useCallback, useMemo } from 'react';
 import './App.scss';
 import { Tree } from './components/Tree/Tree';
 import { MAX_TREES, treesDB } from './models/db';
 import { TreeClass } from './models/Tree';
+import { useObservable } from 'react-use';
+
 
 function App() {
   const trees = useLiveQuery<TreeClass[], TreeClass[]>(async () => (await treesDB.trees.toArray()), [], [])
+  const currentUser = useObservable(treesDB.cloud.currentUser);
   const addTree = useCallback(() => {
     return treesDB.createNewTree()
   }, []);
@@ -19,18 +23,23 @@ function App() {
     return trees.length >= MAX_TREES;
   }, [trees])
 
-  // const login = async () => {
-  //   const res = await treesDB.cloud.login({ grant_type: 'demo', userId: 'foo@demo.local' })
-  //   console.log(res);
-  // }
+  const login = async () => {
+    const res = await treesDB.cloud.login({ grant_type: 'demo', userId: 'foo@demo.local' })
+    console.log(res);
+  }
   return (
     <div className="app-con">
       <nav>
         <h1>Tree Creator</h1>
         <div className="nav-panel">
+          {currentUser?.isLoggedIn ?
+          <div className="loggedIn">
+            Signed in as: <a>{currentUser.name}</a>
+          </div>
+          :<div onClick={login} className={`btn primary ${!trees?.length ? 'disable' : ""}`}>Login</div>
+          }
           <div onClick={() => addTree()} title={`${maxAchived ? `Limited to ${MAX_TREES} trees. Please first delete a tree in order to be able to add another ` : 'Add a new tree'}`} className={`btn primary ${maxAchived ? 'disable' : ""}`}>Add Tree</div>
           <div onClick={() => deleteAllTrees()} className={`btn primary ${!trees?.length ? 'disable' : ""}`}>Delete All Trees</div>
-          {/* <div onClick={login} className={`btn primary ${!trees?.length ? 'disable' : ""}`}>Login</div> */}
         </div>
       </nav>
       <div className="trees-con">
