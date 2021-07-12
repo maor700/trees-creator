bd740269a741fab769201c0872ec405fae3334d9
Maor Elimelech <maor700@gmail.com>
2021-07-12 18:48:03 +0300

    fea :login
    fea: upgraded dexies packages

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { useCallback, useMemo } from 'react';
 import { MAX_TREES, treesDB } from './models/treesDb';
 import { TreeClass } from './models/Tree';
 import { CategoryTree } from './components/CategoryTree/CategoryTree';
+import { useObservable } from 'react-use';
 import './App.scss';
 
 function App() {
   const trees = useLiveQuery<TreeClass[], TreeClass[]>(async () => (await treesDB.trees.toArray()), [], [])
-
+  const currentUser = useObservable(treesDB.cloud.currentUser);
   const addTree = useCallback(() => {
     return treesDB.createNewTree()
   }, []);
@@ -20,11 +21,22 @@ function App() {
     return trees.length >= MAX_TREES;
   }, [trees])
 
+  const login = async () => {
+    const res = await treesDB.cloud.login({ grant_type: 'demo', userId: 'foo@demo.local' })
+    console.log(res);
+  }
+
   return (
     <div className="app-con">
       <nav>
         <h1>Tree Creator</h1>
         <div className="nav-panel">
+          {currentUser?.isLoggedIn ?
+            <div className="loggedIn">
+              Signed in as: <a>{currentUser.name}</a>
+            </div>
+            : <div onClick={login} className={`btn primary ${!trees?.length ? 'disable' : ""}`}>Login</div>
+          }
           <div onClick={() => addTree()} title={`${maxAchived ? `Limited to ${MAX_TREES} trees. Please first delete a tree in order to be able to add another ` : 'Add a new tree'}`} className={`btn primary ${maxAchived ? 'disable' : ""}`}>Add Tree</div>
           <div onClick={() => deleteAllTrees()} className={`btn primary ${!trees?.length ? 'disable' : ""}`}>Delete All Trees</div>
         </div>
